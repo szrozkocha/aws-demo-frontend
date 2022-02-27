b31afe5cbbe85621cabb1456dde12d387eb49dd8
Szymon Rozkocha <rozkocha@wszib.edu.pl>
2022-02-27 17:48:59 +0100

    app init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+interface StatusResponse {
+  message: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'aws-demo-frontend';
+export class AppComponent implements OnInit {
+  message: string = "(no message)"
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get<StatusResponse>("/api/status").subscribe({
+      next: response => this.message = response.message,
+      error: err => this.message = "Error(status: " + err.status + ", message: " + err.message + ")"
+    });
+  }
 }
